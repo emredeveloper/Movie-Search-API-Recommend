b62b1d83c3d41b00767f0d210fd7c2e17441c1a1
emredeveloper <karatasqemre@gmail.com>
2024-07-09 19:14:55 +0300

    1.4

--- src/geminiapi.js
+++ src/geminiapi.js
@@ -5,7 +5,7 @@ const genAI = new GoogleGenerativeAI(api_key);
 
 async function generateFilmRecommendations(film, rating) {
     try {
-        const model = genAI.getGenerativeModel({ model: "gemini-pro-latest" });
+        const model = genAI.getGenerativeModel({ model: "gemini-1.0-pro-latest" });
 
         const prompt = `
 🎬 **Film Önerisi**
