e0ae35589072aac7826cd355abce65b7993565b6
emredeveloper <karatasqemre@gmail.com>
2024-07-09 19:08:51 +0300

    1.3

--- src/FilmSearch.js
+++ src/FilmSearch.js
@@ -1,6 +1,5 @@
-// src/FilmSearch.js
 import React, { useState, useEffect } from 'react';
-import {  Container, Card, Button } from 'react-bootstrap';
+import { Container, Card, Button } from 'react-bootstrap';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import FilmDetail from './FilmDetail';
@@ -10,11 +9,7 @@ import './FilmSearch.css';
 import './App.css';
 import MyNavbar from "./navbar";
 
-//const fs = require('fs');
-
 const omdbApiKey = process.env.REACT_APP_OMDB_API_KEY;
-const timezoneApiKey = process.env.REACT_APP_TIMEZONE_API_KEY; // Yeni eklenen API anahtarı
-
 
 const buttonStyleOner = {
     backgroundColor: '#051728',
@@ -43,19 +38,10 @@ const FilmSearch = () => {
     const [films, setFilms] = useState([]);
     const [selectedFilm, setSelectedFilm] = useState(null);
     const [recommendations, setRecommendations] = useState('');
-    const [showAlert, setShowAlert] = useState(true);
-    const [confirmChecked, setConfirmChecked] = useState(false);
-    const [userName, setUserName] = useState('');
-    const [currentTime, setCurrentTime] = useState('');
-
 
     useEffect(() => {
         const container = document.getElementById('filmSearchContainer');
         container.classList.add('explosion-container');
-    }, []);
-
-    useEffect(() => {
-        timezone().then(r => console.log(r));
     }, []);
 
     const searchFilms = async () => {
@@ -63,72 +49,24 @@ const FilmSearch = () => {
             const response = await axios.get(`https://www.omdbapi.com/?apikey=${omdbApiKey}&s=${search}`);
 
             if (response.data.Response === 'True') {
-                // Check if the response indicates success
                 setFilms(response.data.Search || []);
             } else {
-                // Handle the case where the API response indicates an error
                 console.error('Film arama hatası:', response.data.Error);
             }
         } catch (error) {
-            // Handle general request errors
             console.error('Film arama hatası:', error.message);
         }
     };
-    /**
-     * const closeAlert = () => {
-     *         setShowAlert(false);
-     *     };
-     */
-
 
     const getFilmRecommendations = async () => {
         try {
             const recommendations = await generateFilmRecommendations(search);
             setRecommendations(recommendations);
-            timezone().then(r => console.log(r)); // Film önerileri alındıktan sonra zaman bilgisini al
         } catch (error) {
             console.error('Film önerileri alınırken bir hata oluştu:', error);
         }
     };
 
-    const timestyle = {
-        backgroundColor: '#931bbb',
-        color: '#fff',
-        padding: '2px 10px',
-        borderRadius: '15px',
-
-    }
-    const timezone = async (City = "Istanbul",Country = "Turkey") => {
-        try {
-            const response = await axios.get(`https://timezone.abstractapi.com/v1/current_time/?api_key=${timezoneApiKey}&location=${City},${Country}`);
-            setCurrentTime(response.data);//if(response.status === 200){
-             //  fs.writeFileSync('output.json',JSON.stringify(response.data) );
-                console.log(response.data["latitude"]);
-
-
-        } catch (error) {
-            console.log(error);
-        }
-    };
-
-
-
-
-    const confirmAndProceed = () => {
-        if (userName && confirmChecked) {
-            setShowAlert(false);
-        }
-    };
-    const showWelcomeMessage = () => {
-        if (!showAlert && userName) {
-            return (
-                <div className="welcome-message">
-                    Hoş geldin, {userName}!
-                </div>
-            );
-        }
-        return null;
-    };
     const downloadRecommendations = () => {
         const element = document.createElement('a');
         const file = new Blob([recommendations], { type: 'text/plain' });
@@ -164,66 +102,12 @@ const FilmSearch = () => {
         setSelectedFilm(null);
     };
 
-
     return (
-
         <div id="filmSearchContainer" className="mt-4">
-
-            {showAlert && (
-                <div className="alert-container">
-                    <div className="alert-content">
-                        <p>İlk önce film arama kutusuna basıp "Ara" demelisiniz. Daha sonra öneri için "Film Öner!!"
-                            tuşuna tıklayabilirsiniz. Film Önerisinden faydalanmak için izlediğiniz filmin ismini tam
-                            yazmanız iyi olacaktır</p>
-                        <label htmlFor="userName">İsminiz: </label>
-                        <input
-                            type="text"
-                            id="userName"
-                            value={userName}
-                            onChange={(e) => setUserName(e.target.value)}
-                        />
-                        <label style={{display: 'flex', alignItems: 'center', marginBottom: '15px', color: '#051728'}}>
-                            <input
-                                style={{
-
-                                    width: '20px',
-                                    height: '20px',
-                                    borderRadius: '4px',
-                                    border: '2px solid red',
-                                    backgroundColor: confirmChecked ? 'green' : 'green',
-                                    marginRight: '8px',
-                                    cursor: 'pointer',
-
-                                }}
-                                type="checkbox"
-                                checked={confirmChecked}
-                                onChange={() => setConfirmChecked(!confirmChecked)}
-                            />
-                            <span style={{fontSize: '16px', color: '#f5faff'}}>Onaylıyorum</span>
-                        </label>
-
-
-                        <button type="button" className="btn btn-primary" onClick={confirmAndProceed}>
-                            Onayla
-                        </button>
-
-                    </div>
-                </div>
-            )}
-
-
-            {/* Welcome message */}
-            {showWelcomeMessage()}
-            <div>
-
-                <h2 style={timestyle}>{currentTime && currentTime.datetime}</h2>
-            </div>
-            <MyNavbar/>
-
+            <MyNavbar />
 
             <div style={{margin: '20px 0'}}></div>
             <Container>
-
                 <h1 className="mb-4">Film Arama</h1>
 
                 <div className="input-group mb-lg-4">
@@ -235,8 +119,7 @@ const FilmSearch = () => {
                         onChange={(e) => setSearch(e.target.value)}
                     />
                     <div className="input-group-append">
-                        <button className="btn btn-outline-secondary" type="button" onClick={searchFilms}
-                                style={buttonStyleAra}>
+                        <button className="btn btn-outline-secondary" type="button" onClick={searchFilms} style={buttonStyleAra}>
                             Ara
                         </button>
                     </div>
@@ -246,10 +129,9 @@ const FilmSearch = () => {
                     <label
                         style={{
                             marginRight: '10px',
-                            fontWeight: 'bold', // Örnek: Metin kalın olsun
-                            color: '#070606', // Örnek: Metin rengi
-                            fontSize: '14px', // Örnek: Metin boyutu
-                            // Diğer stil özellikleri eklenebilir
+                            fontWeight: 'bold',
+                            color: '#070606',
+                            fontSize: '14px',
                         }}
                         htmlFor="filmInput"
                     >
@@ -274,7 +156,6 @@ const FilmSearch = () => {
                                         </Card.Text>
                                     </Card.Body>
 
-
                                     <Button
                                         style={{ backgroundColor: '#e8c1c1', color: '#000000', padding: '8px 16px', borderRadius: '15px', cursor: 'pointer', fontSize: '16px', fontWeight: 'bold', margin: '6px' }}
                                         variant="primary"
@@ -283,7 +164,6 @@ const FilmSearch = () => {
                                     >
                                         Önerileri Bilgisayara İndir!
                                     </Button>
-
                                 </Card.Body>
                             </Card>
                         </div>
@@ -322,8 +202,6 @@ const FilmSearch = () => {
                                 </Card>
                             </div>
                         ))}
-
-
                     </div>
                 </div>
 
@@ -332,8 +210,7 @@ const FilmSearch = () => {
 
             <div className="social-links text-center">
                 <a href="https://www.linkedin.com/in/cihatemrekaratas/" target="_blank" rel="noopener noreferrer">
-                    <i className="fab fa-linkedin-in fa-2x mx-3 social-icon"
-                       style={{color: '#0077B5', transition: 'transform 0.9s ease-in-out'}}></i>
+                    <i className="fab fa-linkedin-in fa-2x mx-3 social-icon" style={{color: '#0077B5', transition: 'transform 0.9s ease-in-out'}}></i>
                 </a>
                 <a href="https://github.com/emredeveloper" target="_blank" rel="noopener noreferrer">
                     <i className="fab fa-github fa-2x mx-3 social-icon" style={{color: '#24292e'}}></i>
@@ -342,8 +219,6 @@ const FilmSearch = () => {
                     <i className="fab fa-kaggle fa-2x mx-3 social-icon" style={{color: '#20BEFF'}}></i>
                 </a>
             </div>
-
-
         </div>
     );
 };
